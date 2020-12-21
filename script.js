69141099fee0460edac64a3ec832222e803d29af
Danila <Gdanila2002@yandex.ru>
2020-12-21 22:31:20 +0300

    задание 2

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const sheepCount=prompt('Сколько овец нужно посчитать?'
 for (let i=0; i<sheepCount; i++){
     console.log((i+1)+' овечка...');
 }
+
+let number = 0;
+do{
+    if(number%2==0){
+        console.log(number+" четное");
+    }else{
+        console.log(number+" нечетное");
+    }
+    number++;
+} while(number<=15);
